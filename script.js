159a7056d6fa11f449cb7494502ba443515e8f67
GANNE ROHAN <gannerohan@gmail.com>
2025-07-23 22:27:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bored(){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("bored").textContent = data.activity
+    })
+}
